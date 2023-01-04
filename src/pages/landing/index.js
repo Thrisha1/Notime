82b128e7a11583ac22f77436ea87c54b5c483cbc
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2023-01-04 21:24:55 +0530

    first commit

---
+++ src/pages/landing/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hai from "../../common/UI/Dashboard/components/Hai";
+
+const Landing = () => {
+  return (
+    <div>
+      <Hai />
+    </div>
+  );
+};
+
+export default Landing;
---
+++ src/pages/signin/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signin from '../../common/UI/signin/Signin'
+
+const index = () => {
+  return (
+    <Signin/>
+  )
+}
+
+export default index
