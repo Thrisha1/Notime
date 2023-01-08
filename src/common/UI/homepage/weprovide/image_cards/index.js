05c6d7756df1cdf41ee0f973f426d3966a593dab
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2023-01-08 13:28:32 +0530

    Navbar almost solved commented img

--- src/common/UI/homepage/weprovide/image_cards/index.js
+++ src/common/UI/homepage/weprovide/image_cards/index.js
@@ -8,14 +8,14 @@ function index({name,img}) {
 
     <div class="relative flex justify-center  max-w-sm h-full rounded-3xl shadow-md bg-transparent">
 
-      <Image
+      {/* <Image
 
         src={Cardbg}
         alt="Picture of the author"
         width={500}
         height={500}
         className=" hover:bg-gray-900 rounded-[35px]"
-      />
+      /> */}
       <Image src={img} className="absolute top-10 hover:rotate-45" alt="" />
       <div className="absolute top-1/2 h-full text-center ">
           <button className="text-white text-3xl tracking-[10px] font-bold">
