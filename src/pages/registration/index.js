a3eebdc62fe2e3f08dc06e3eae49d2a984ad3b63
Ambady <cssreedeep@gmail.com>
2023-09-23 23:56:08 +0530

    feat : Added enrolement feature

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-const index = () => {
+const Index = () => {
     return (
         <div>
             {/*<Courses />*/}
-            registration
+            <iframe
+                className="airtable-embed"
+                src="https://airtable.com/embed/app6CHsYoG1ehpXei/shr6X0LRdOp2Ad9NC?backgroundColor=grayDusty"
+                frameBorder="0"
+                onWheel={() => {}} // You can handle mouse wheel events here if needed
+                width="100%"
+                height="533"
+                style={{ background: "transparent", border: "1px solid #ccc" }}
+            ></iframe>
         </div>
     );
 };
 
-export default index;
+export default Index;
