920305b0cc86f38fe35ad5cc104efb52138b940e
Shyamnarayananj <jshyamnarayanan@gmail.com>
2023-09-24 00:35:16 +0530

    Merge branch 'main' of https://github.com/Thrisha1/Notime

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const index = () => {
     return (
         <div>
-            {/*<Courses />*/}
+            {<registration/>}
             registration
         </div>
     );
