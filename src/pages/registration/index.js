2a0363d44306ea91e761b910399068f15a4b65a5
DareDevilStudios <studios.daredevil@gmail.com>
2023-09-24 22:46:44 +0530

    course details finished successfully

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -2,6 +2,7 @@ import React, { useState , useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Airtable from 'airtable';
 import { useRouter } from "next/router";
+import Navbar from "../../common/components/Navbar";
 
 
 const Index = () => {
@@ -42,6 +43,7 @@ const Index = () => {
     }
     return (
         <div>
+            <Navbar/>
             <section class="bg-transparent py-12">
                 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
                     {/* <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
@@ -86,7 +88,7 @@ const Index = () => {
                                     <label for="github" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Github URL</label>
                                     <input {...register("github")} type="text" name="github" id="github" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://github.com/"  />
                                 </div>
-                                <button type="submit" class="w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center">Enroll Now</button>
+                                <button type="submit" class="w-full bg-white text-black font-medium rounded-lg text-sm px-5 py-2.5 text-center">Enroll Now</button>
                             </form>
                         </div>
                     </div>
