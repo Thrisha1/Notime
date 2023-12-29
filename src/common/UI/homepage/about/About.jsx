5d5dfa75787f4f9723573dec90e48361e954864a
Thrisha1 <thrishakannan25@gmail.com>
2023-12-29 11:54:35 +0530

    sanity setup completed

--- src/common/UI/homepage/about/About.jsx
+++ src/common/UI/homepage/about/About.jsx
@@ -4,7 +4,7 @@ import Navbar from "../../../components/Navbar";
 function About() {
   return (
       <>
-    <div id="whynotime" className="flex flex-col-reverse justify-center items-center text-justify md:text-left md:flex-row w-full my-32 ">
+    <div id="whynotime" className="flex flex-col-reverse justify-center items-center text-justify md:text-left md:flex-row w-full my-32 h-full">
       <div className="w-5/6 md:w-1/2 flex justify-center items-center text-white">
         <p className="w-100 md:w-5/6">
           NoTime is the ultimate destination for those who want to make the most
