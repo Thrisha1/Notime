47ccb2ac4de8437feffd4407f4d192c9c2edef46
DareDevilStudios <studios.daredevil@gmail.com>
2024-01-25 18:59:13 +0530

    getStaticProps is moved to page to get the data : issue fixed of revalidate

--- src/pages/courses/index.js
+++ src/pages/courses/index.js
@@ -1,13 +1,52 @@
 import React from "react";
 import Courses from "../../common/UI/Courses/Courses";
+import { client } from "../../../sanity/lib/client";
 
-const Index = () => {
+const Index = ({courseList}) => {
+  console.log("courses", courseList);
   return (
     <div className="overflow-x-hidden">
       
-     <Courses />
+     <Courses courseList={courseList} />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const query = `*[_type == "course"] {
+    title,
+    slug,
+    description,
+    courseImage {
+      asset -> {
+        _id,
+        url
+      }
+    },
+    learningObjectives,
+    topics,
+    prerequisites,
+    mentors
+  }`;
+
+  try {
+    const data = await client.fetch(query);
+
+    return {
+      props: {
+        courseList: data,
+      },
+      revalidate: 60 * 60 * 24, // Revalidate every 24 hours
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        courseList: [],
+      },
+      revalidate: 60 * 5, // Revalidate every 5 minutes in case of an error
+    };
+  }
+}
 
 export default Index;
